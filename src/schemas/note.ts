f756f30e4851378cdcc95e3edb82093f1f81c2b1
DimaTimoschenko02 <dim.timoschenko2015@gmail.com>
2022-09-18 18:12:11 +0300

    fix bug in word spelling:)

--- src/schemas/note.ts
+++ src/schemas/note.ts
@@ -3,5 +3,5 @@ import { object, string } from "yup";
 export const noteSchema = object({
   name: string().trim().required().max(20).min(3),
   content: string().trim().required(),
-  category: string().oneOf(["task", "qoute", "random", "idea"]),
+  category: string().oneOf(["task", "quote", "random", "idea"]),
 });
